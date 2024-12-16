b73d055dc0617f5f975665bdc919e05b8a4d039e
zj <zhoujing382@gmail.com>
2024-12-16 10:46:10 +0800

    把代理ip和端口写入文件中

--- get_proxy.py
+++ get_proxy.py
@@ -14,7 +14,8 @@ if __name__ == '__main__':
     # copy the page content then copy into  proxy_pages/proxy_pagesv1.txt
     # ran this scripts
     # 是否可用， 并打印可用的ip和端口
-    with open('./proxy_pages/proxy_pagesv1.txt', mode='rt', encoding='utf8') as f:
+    ip_port = []
+    with open('./proxy_pages/proxy_pagesv1.txt', mode='rt', encoding='utf8') as f, open('./proxy_list.txt', mode='w', encoding='utf8') as f2:
         for line in f:
             mt = re.match('(\d+\.\d+\.\d+\.\d+)\t(\d+)\t\w+\t.*\t.*\n', line)
             if mt is not None:
@@ -25,8 +26,9 @@ if __name__ == '__main__':
                     'https': proxy
                 }
                 try:
+                    # 可以使用aiohttp 获得更高的并法
                     resp = requests.get(url, headers=headers, proxies=proxies, timeout=10)
                     if resp.status_code == 200:
-                        print(ip_port)
+                        f2.write(ip_port + '\n')
                 except Exception as e:
                     pass
